ca55a1ee4d97fa01b490de789793f686108b83fa
jbrachey3 <jbrachey3@gatech.edu>
2021-10-23 13:08:48 -0400

    Add some user sign up ui

---
+++ frontend/src/sign-up/farm-sign-up.js
@@ -0,0 +1,13 @@
+import { registerUser } from "./user-sign-up"
+
+const registerFarm = () => {
+
+}
+
+export const FarmSignUp = () => {
+    return (
+        <div>
+            <span>Register your farm!</span>
+        </div>
+    )
+}
