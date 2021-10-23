48ab48c6395760b4c6ac04561fa06e31a677c1af
jbrachey3 <jbrachey3@gatech.edu>
2021-10-23 01:46:28 -0400

    Add some frontend structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-import Farm from './farm-page/farm';
-import FarmPage from './farm-page/components/main-farm-page';
+//import Farm from './farm-page/farm';
+//import FarmPage from './farm-page/components/main-farm-page';
 
 function App() {
-  const farm = new Farm('Local Organic Farm', '1234567890', '123 Farm Lane', 'Atlanta', 'GA', '30269');
+  //const farm = new Farm('Local Organic Farm', '1234567890', '123 Farm Lane', 'Atlanta', 'GA', '30269');
   return (
     <div className="App">
-      <FarmPage farm={farm}/>
     </div>
   );
 }
