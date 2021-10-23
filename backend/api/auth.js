5fa72f1cdbccc7b46df3f0b54596ea07a7b9d7b0
usjpin <65671230+usjpin@users.noreply.github.com>
2021-10-23 00:09:23 -0500

    added backend

---
+++ backend/api/auth.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const authRouter = express.Router()
+
+authRouter.post('/login', (req, res) => {
+
+})
+
+authRouter.get('/login', (req, res) => {
+
+})
+
+module.exports = authRouter
---
+++ backend/api/posts.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const postsRouter = express.Router()
+
+postsRouter.post('/create', (req, res) => {
+
+})
+
+postsRouter.get('/get', (req, res) => {
+
+})
+
+postsRouter.put('/update', (req, res) => {
+
+})
+
+postsRouter.delete('/delete/:id', (req, res) => {
+
+})
+
+
+module.exports = postsRouter
