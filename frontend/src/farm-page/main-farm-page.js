7112b4bb142fff08b124aa4dce03aa31457e5934
dforester8 <70494564+dforester8@users.noreply.github.com>
2021-10-24 04:00:28 -0400

    Farm page

--- frontend/src/farm-page/main-farm-page.js
+++ frontend/src/farm-page/main-farm-page.js
@@ -1,6 +1,8 @@
 import FarmPageHeader from "./header";
 import axios from "axios";
 import MainHeader from "../header/main-header";
+import FollowButton from "./follow-button";
+import './main-farm-page-style.css';
 
 export const getFarm = async (farmName) => {
     return axios.get(`http://localhost:8000/api/farms/get/${farmName}`)
@@ -11,14 +13,37 @@ export const getFarm = async (farmName) => {
         });
 }
 
-const FarmPage = ({url}) => {
-    const farmName = url.location.pathname.substring(1);
-    const farm = getFarm(farmName);
+const FarmPage = ({farm}) => {
+    //const farmName = url.location.pathname.substring(1);
+    //const farm = getFarm(farmName);
+    console.log('farm', farm);
     return (
         <div>
             <MainHeader />
-            <FarmPageHeader name={farm.name} phone={farm.phone}
-                address={farm.address} city={farm.city} state={farm.state} zip={farm.zip}/>
+            <div className = "farmPage farmPageInfo">
+                <div className = "nameFarmPage">
+                    <label>{farm.name}</label>
+                </div>
+                <div className = "phonelocationFarmPage">
+                    <label>{farm.phone}</label>
+                    {farm.location.address}, {farm.location.city}, {farm.location.state}, {farm.location.zip}
+                </div>
+                <div className = "infoFPContain">
+                    <label className = "infoFP">Additional info: </label>
+                    {farm.info}
+                </div>
+                <div className = "sustainFPContain">
+                    <label className = "sustainFP">What do you do for sustainability? </label>
+                    {farm.sustainability}
+                </div>
+                <div className = "hoursFPContain">
+                    <label className = "hoursFP">What are your hours? </label>
+                    {farm.hours}
+                </div>
+                <div>
+                    {true ? (<button className = "subButton">Follow</button>) : (<button className = "alreadyFollowed">Followed<div id = "tick-mark"></div></button>)}
+                </div>
+            </div>
         </div>
     );
 }
